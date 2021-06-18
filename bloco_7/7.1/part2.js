2a4a34fef18f6cb61f3eb4279f1c0758adf2acd6
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-06-18 20:53:02 -0300

    parte 2 exercicio 7.1

--- bloco_7/7.1/part2.js
+++ bloco_7/7.1/part2.js
@@ -1,12 +1,32 @@
 //exercício 1
-const fatorial = (numero) => {
-  let fatorial = numero;
-  for(let i = numero - 1; i >= 1; i -= 1) {
-    fatorial *= [i]
-  }
-  return numero + '! = ' + fatorial
+// const fatorial = (numero) => {
+//   let fatorial = numero;
+//   for(let i = numero - 1; i >= 1; i -= 1) {
+//     fatorial *= [i]
+//   }
+//   return `${numero}! = ${fatorial}`
+// }
+
+// console.log(fatorial(6))
+
+//exercício 2 
+
+const maiorPalavra = (palavra) => {
+  let arrString = palavra.split(" ");
+  let maior = 0;
+  let maiorPalavra = null;
+  arrString.forEach(function(str) {
+    console.log(str)
+    if (maior < str.length) {
+      maior = str.length;
+      maiorPalavra = str;
+    }
+  });
+  return maiorPalavra;
 }
 
-console.log(fatorial(6))
 
+
+
+console.log(maiorPalavra('teu cu é bem lixo seu arrombado'));
 
