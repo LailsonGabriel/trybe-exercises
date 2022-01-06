a852b715802d6d97c455043a279804dda3d311e6
LailsonGabriel <lailsongabrielturbo@gmail.com>
2022-01-06 17:35:47 -0300

    exercicio 3, dia 22.3

--- Back-end/bloco_22/22.3/exercise1.js
+++ Back-end/bloco_22/22.3/exercise1.js
@@ -1,6 +1,7 @@
 const whatCondition = (number) => {
   if (number < 0) return "negativo";
   if (number > 0) return "positivo";
+  if (typeof number !== "number") return "o valor deve ser um número";
   return "neutro";
 };
 
--- Back-end/bloco_22/22.3/tests/exercise1.test.js
+++ Back-end/bloco_22/22.3/tests/exercise1.test.js
@@ -1,8 +1,13 @@
 const { expect } = require("chai");
 const whatCodition = require("../exercise1");
 
-const retornoFuncao = ["positivo", "negativo", "neutro"];
-const [positivo, negativo, neutro] = retornoFuncao;
+const retornoFuncao = [
+  "positivo",
+  "negativo",
+  "neutro",
+  "o valor deve ser um número",
+];
+const [positivo, negativo, neutro, notNumber] = retornoFuncao;
 
 describe("Se o número passado for maior que 0", () => {
   it("deverá retornar positivo", () => {
@@ -22,5 +27,12 @@ describe("Se o número passado for igual a 0", () => {
   it("deverá retornar neutro", () => {
     const result = whatCodition(0);
     expect(result).to.be.equals(neutro);
+  });
+});
+
+describe("Se o paramêtro passado não for igual um número", () => {
+  it("deverá retornar um error", () => {
+    const result = whatCodition("teste");
+    expect(result).to.be.equals(notNumber);
   });
 });
