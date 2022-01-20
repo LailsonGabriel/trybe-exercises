be6f98a20e5b1e01e1576a35d64ea0ef9695a4d6
Lailson Cavalcante <lailson.cavalcante@tegrus.com.br>
2022-01-20 16:41:39 -0300

    status code de sucesso adicionado

--- Back-end/bloco_23/22.3/refactoring/controllers/productController.js
+++ Back-end/bloco_23/22.3/refactoring/controllers/productController.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get("/", async (_req, res, _next) => {
   const products = await ProductService.getAll();
 
-  res.send(products);
+  res.status(200).send(products);
 });
 
 router.get("/:id", async (req, res, next) => {
   const product = await ProductService.getById(req.params.id);
 
-  res.send(product);
+  res.status(200).send(product);
 });
 
 router.post("/", async (req, res) => {
@@ -20,13 +20,13 @@ router.post("/", async (req, res) => {
 
   const newProduct = await ProductService.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).send(newProduct);
 });
 
 router.delete("/:id", async (req, res) => {
   const products = await ProductService.exclude(req.params.id);
 
-  res.send(products);
+  res.status(204).send(products);
 });
 
 router.put("/:id", async (req, res) => {
@@ -34,7 +34,7 @@ router.put("/:id", async (req, res) => {
 
   const products = await ProductService.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(204).send(products);
 });
 
 module.exports = router;
